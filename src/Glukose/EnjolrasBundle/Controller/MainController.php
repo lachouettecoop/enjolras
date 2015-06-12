f76c3898fcc9ce1c4cfd9117a8ae7ca569c78138
Clement <homzon@gmail.com>
2015-06-12 11:37:59 +0200

    voting page + dynamic lists

--- src/Glukose/EnjolrasBundle/Controller/MainController.php
+++ src/Glukose/EnjolrasBundle/Controller/MainController.php
@@ -5,10 +5,12 @@ namespace Glukose\EnjolrasBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\HttpFoundation\Request;
+use Condorcet\Condorcet;
+use Condorcet\Vote;
 
 class MainController extends Controller
 {
-    
+
     /**
      * Main action, displays home page
      * 
@@ -18,8 +20,8 @@ class MainController extends Controller
     {
         return $this->render('GlukoseEnjolrasBundle:Main:index.html.twig');
     }
-    
-    
+
+
     /**
      * Show a list of all the subjects available
      * 
@@ -36,14 +38,14 @@ class MainController extends Controller
 
         $subjects = $repository->findByTermine(false);
         $subjectsTermine = $repository->findByTermine(true);
-        
+
         return $this->render('GlukoseEnjolrasBundle:Main:showAllSubjects.html.twig',
                              array('subjects' => $subjects,
-                                  'subjectsTermine' => $subjectsTermine)
+                                   'subjectsTermine' => $subjectsTermine)
                             );
     }
-    
-    
+
+
     /**
      * Show a subject
      * 
@@ -52,7 +54,7 @@ class MainController extends Controller
      */
     public function showSubjectAction($idSubject)
     {
-        //get all the subjects
+        //find selected subject
         $repository = $this
             ->getDoctrine()
             ->getManager()
@@ -60,14 +62,65 @@ class MainController extends Controller
             ;
 
         $subject = $repository->find($idSubject);
-        
+
+        $condorcet = new Condorcet();
+        $condorcet->addCandidate('Wagner');
+        $condorcet->addCandidate('Debussy');
+        $condorcet->addCandidate('Varese');
+        $condorcet->addCandidate('Mozart');
+
+        $vote1 = new Vote ('Mozart>Debussy=Wagner>Varese');
+        $vote2 = new Vote ('Debussy>Wagner=Mozart>Varese');
+        $vote3 = new Vote ('Varese>Debussy=Mozart>Wagner');
+        $vote4 = new Vote ('Mozart>Varese=Wagner>Debussy');
+        //$vote5 = new Vote ('Mozart>Varese=Wagner>Debussy');
+        //$vote6 = new Vote ('Mozart>Varese=Wagner>Debussy');
+        $condorcet->addVote($vote1);
+        $condorcet->addVote($vote2);
+        $condorcet->addVote($vote3);
+        $condorcet->addVote($vote4);
+        //$condorcet->addVote($vote5);
+        //$condorcet->addVote($vote6);
+
+        $result = $condorcet->getWinner();
+        $resultLooser = $condorcet->getLoser();
+
         if($subject){
             return $this->render('GlukoseEnjolrasBundle:Main:showSubject.html.twig',
-                             array('subject' => $subject)
-                            );
+                                 array('subject' => $subject, 'result' => $result, 'looser' => $resultLooser)
+                                );
         }else{
             throw new NotFoundHttpException("Page not found");
         }                
     }
-    
+
+
+    /**
+     * Vote on a subject using condorcet Method
+     * 
+     * @param  integer  $idSubject Id of the selected subject
+     * @return Response 
+     */
+    public function voteCondorcetAction($idSubject)
+    {
+        //find selected subject
+        $repository = $this
+            ->getDoctrine()
+            ->getManager()
+            ->getRepository('GlukoseEnjolrasBundle:Subject')
+            ;
+
+        $subject = $repository->find($idSubject);
+
+
+        if($subject){
+            return $this->render('GlukoseEnjolrasBundle:Main:voteCondorcet.html.twig',
+                                 array('subject' => $subject)
+                                );
+        }else{
+            throw new NotFoundHttpException("Page not found");
+        }
+
+    }
+
 }
